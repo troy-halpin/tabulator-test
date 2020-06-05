465c0a71490bc66a1c5b2a51365a140e2d0b8028
Troy Halpin <troy.halpin@procore.com>
2020-06-05 13:58:17 -0700

    comparing vanilla Tabulator vs React wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react'
+import { Button, Font } from '@procore/core-react'
+import { ReactTabulator } from 'react-tabulator'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import 'tabulator-tables/dist/css/tabulator.min.css'
+import logo from './logo.svg'
+import './App.css'
+
+const Tabulator = require('tabulator-tables')
+
+const data = Array(200).fill({
+  id: 1,
+  name: 'Bob',
+  age: '12',
+  col: 'red',
+  dob: '1/1/2020',
+})
+
+const columns = [
+  { title: 'Name', field: 'name', width: 150 },
+  {
+    title: 'Age',
+    field: 'age',
+    align: 'left',
+    formatter: function (
+      cell: Tabulator.CellComponent,
+      formatterParams: Tabulator.FormatterParams,
+      onRendered: () => void
+    ) {
+      // The function must return the contents of the cell,
+      // either the text value of the cell, valid HTML or a DOM node.
+
+      return <Font>`The age is: ${cell.getValue()}`</Font>
+    },
+  },
+  { title: 'Favourite Color', field: 'col' },
+  { title: 'Date Of Birth', field: 'dob', align: 'center' },
+  {
+    title: 'Passed?',
+    field: 'passed',
+    align: 'center',
+    formatter: 'tickCross',
+  },
+]
+
+function VanillaTabulator(props: any) {
+  let refTable: HTMLDivElement | null = null
+
+  React.useEffect(() => {
+    new Tabulator(refTable, {
+      data,
+      reactiveData: true,
+      columns: columns,
+    })
+  }, [refTable])
+
+  return <div ref={(el) => (refTable = el)} style={{ height: '1000px' }} />
 }
 
-export default App;
+function ReactTabulatorWrapper(props: any) {
+  return (
+    <ReactTabulator
+      data={data}
+      columns={columns}
+      options={{ height: '100vh', movableRows: true }}
+      // this is the only part of the wrapper that accepts jsx afaik
+      footerElement={<Button>Custom Button</Button>}
+    />
+  )
+}
+
+function App() {
+  // return <VanillaTabulator />
+  return <ReactTabulatorWrapper />
+}
+
+export default App
